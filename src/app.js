d69ca75ccfa803fee57f09a33cb5eedd4227288a
Mikhail Medinskiy <medinskiym@gmail.com>
2018-08-29 18:11:05 +0300

    AL-2 remove module css, they are breaking antd

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'antd';
 import './styles/styles.scss';
-import styles from './styles/App.scss';
 
 
 const TestMe = () => (
   <div>
-    <header className={styles.header}>
-      {'some text header, modul test'}
-    </header>
     <input type="text" />
     <p>
       {'some text'}
+      <Button type="primary">Primary</Button>
     </p>
   </div>
 );
