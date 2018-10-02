1c25b89db34bccb576e2754ef5aae81cb1a60e12
Unknown <medinskiym@gmail.com>
2018-10-02 18:39:22 +0300

    AL-196 rewrite loading page

--- src/app.js
+++ src/app.js
@@ -9,30 +9,30 @@ import { login } from './Login/actions/auth';
 const store = configureStore();
 
 const jsx = (
-    <Provider store={store}>
-      <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 let hasRenderred = false;
 const renderApp = () => {
-    if (!hasRenderred) {
-        ReactDOM.render(jsx, document.getElementById('app'));
-        hasRenderred = true;
-    }
+  if (!hasRenderred) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRenderred = true;
+  }
 };
 
 const user = JSON.parse(localStorage.getItem('userInfo'));
 const currentPageUrl = history.location.pathname;
 
- const loginSet = [
+const loginSet = [
   { user: true, page: '/', response: '/wordgroups' },
   { user: true, page: '/registration', response: '/wordgroups' },
   { user: true, page: currentPageUrl, response: currentPageUrl },
   { user: false, page: '/', response: '/' },
   { user: false, page: '/registration', response: '/registration' },
   { user: false, page: currentPageUrl, response: '/' },
-]
+];
 
 const result = loginSet.find(item => item.user == !!user && item.page === currentPageUrl)
 
