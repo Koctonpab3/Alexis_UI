e49415fc45ad1e80cd3e753863a3c522a539d287
Vika <vika97777@gmail.com>
2018-10-24 20:22:40 +0300

    AL-298 bug fixed

--- src/Words/utils/search.js
+++ src/Words/utils/search.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const searchWords = (text, searchText) => (
   <span>
-    {text.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i')).map((fragment, i) => (
+    {text.split(new RegExp(`(^(?=${searchText}))|(?=${searchText})`, 'i')).map((fragment, i) => (
       fragment.toLowerCase() === searchText.toLowerCase()
                 ? <span key={i} className="highlight">{fragment}</span> : fragment // eslint-disable-line
     ))}
