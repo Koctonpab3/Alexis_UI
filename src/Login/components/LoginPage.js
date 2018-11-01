88c14d491dd29fd09d185f475e2bbc387625eb2a
Medinksiy <medinskiym@gmail.com>
2018-11-01 16:36:06 +0200

    change animation

--- src/Login/components/LoginPage.js
+++ src/Login/components/LoginPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from './Login';
 
 const LoginPage = () => (
-  <div className="loginpage page">
+  <div className="loginpage">
     <div className="loginpage__box">
       <Login />
     </div>
